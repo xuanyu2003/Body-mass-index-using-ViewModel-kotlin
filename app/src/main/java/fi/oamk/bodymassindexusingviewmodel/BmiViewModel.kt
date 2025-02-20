6443deeb81658816c92b24d397e1c7b22c2ac3df
xuanyu2003 <t3lixu00@students.oamk.fi>
2025-02-20 17:04:58 +0200

    finished

---
+++ app/src/main/java/fi/oamk/bodymassindexusingviewmodel/BmiViewModel.kt
@@ -0,0 +1,28 @@
+package fi.oamk.bodymassindexusingviewmodel
+
+
+import androidx.lifecycle.ViewModel
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+
+class BmiViewModel : ViewModel() {
+
+    var height by mutableStateOf("")
+    var weight by mutableStateOf("")
+    private var _bmi by mutableStateOf<Float?>(null) // Change to Float?
+
+    val bmi: String
+        get() = _bmi?.let { String.format("%.1f", it) } ?: ""
+
+    fun calculateBmi() {
+        val heightValue = height.toFloatOrNull()
+        val weightValue = weight.toFloatOrNull()
+
+        if (heightValue != null && weightValue != null && heightValue > 0) {
+            _bmi = weightValue / (heightValue * heightValue) // Now correctly stores Float
+        } else {
+            _bmi = null
+        }
+    }
+}
